8ecda2ba7ab32ec5228569ea32c97c0b77f1a14f
Leo_Amv <leonidamvrosov@yandex.ru>
2022-06-23 03:46:54 +0500

    Example005_Condition_IfElse added

---
+++ Example005_Condition_IfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter user name ");
+string username = Console.ReadLine();
+if(username.ToLower() == "leo")
+{
+    Console.WriteLine("This is LEO!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.Write(username);
+}
